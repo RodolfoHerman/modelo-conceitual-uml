bb6986555b770c56224fc70eb567b58f183f6702
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2019-02-22 20:40:52 -0300

    Projeto criado

---
+++ src/test/java/br/com/rodolfo/loja/LojaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.rodolfo.loja;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LojaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

5d09c0064acce1706c6ab387b2a99f56d27c8b4a
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2019-02-23 11:05:01 -0300

    Criando operação de instanciação

--- src/main/java/br/com/rodolfo/loja/LojaApplication.java
+++ src/main/java/br/com/rodolfo/loja/LojaApplication.java
@@ -1,13 +1,32 @@
 package br.com.rodolfo.loja;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.rodolfo.loja.domain.Categoria;
+import br.com.rodolfo.loja.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class LojaApplication {
+public class LojaApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
--- src/main/java/br/com/rodolfo/loja/resources/CategoriaResource.java
+++ src/main/java/br/com/rodolfo/loja/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package br.com.rodolfo.loja.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;

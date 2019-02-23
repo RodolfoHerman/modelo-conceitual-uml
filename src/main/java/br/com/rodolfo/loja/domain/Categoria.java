061984e8810c9cba76bd79ee035f54d594614fe8
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2019-02-23 10:10:40 -0300

    Banco de dados H2 e criação da tabela Categoria

--- src/main/java/br/com/rodolfo/loja/domain/Categoria.java
+++ src/main/java/br/com/rodolfo/loja/domain/Categoria.java
@@ -3,15 +3,24 @@ package br.com.rodolfo.loja.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Categoria
  */
+@Entity
 public class Categoria implements Serializable{
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
 
+    
     private String nome;
 
     public Categoria() {}
